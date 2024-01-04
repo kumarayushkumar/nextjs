928e2751630dbc02e2a6bc3b87e9a62cf33c3212
Ayush kumar <kumarayushkumar@protonmail.com>
2024-01-04 13:12:46 +0530

    not found page

---
+++ src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -0,0 +1,7 @@
+export default function ReviewNotFound() {
+  return (
+    <div>
+      <h1>Review not found</h1>
+    </div>
+  )
+}
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,15 @@
+import { notFound } from 'next/navigation'
+
 export default function Review({
   params
 }: {
   params: { productId: string; reviewId: string }
 }) {
+  if (parseInt(params.reviewId) === 0) {
+    notFound()
+  } else if (parseInt(params.reviewId) > 10) {
+    notFound()
+  }
   return (
     <div>
       <h1>
